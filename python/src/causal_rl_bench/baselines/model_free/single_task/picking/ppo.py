feb0ad679c207a9a440523d92575eca4c4448f1b
ossamaAhmed <ossama.ahmed@mail.mcgill.ca>
2020-06-15 15:10:28 +0200

    added picking baseline

--- python/src/causal_rl_bench/baselines/model_free/single_task/picking/ppo.py
+++ python/src/causal_rl_bench/baselines/model_free/single_task/picking/ppo.py
@@ -31,12 +31,13 @@ def train_policy(num_of_envs, log_relative_path, maximum_episode_length,
 
         set_global_seeds(seed_num)
         return _init
-    os.makedirs(log_relative_path)
-    checkpoint_callback = CheckpointCallback(save_freq=validate_every_timesteps,
-                                             save_path=log_relative_path,
-                                             name_prefix='model')
+
     policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[256, 128])
     env = SubprocVecEnv([_make_env(rank=i) for i in range(num_of_envs)])
+    checkpoint_callback = CheckpointCallback(
+        save_freq=validate_every_timesteps,
+        save_path=log_relative_path,
+        name_prefix='model')
     model = PPO2(MlpPolicy, env, _init_setup_model=True,
                  policy_kwargs=policy_kwargs,
                  verbose=1, **ppo_config)
@@ -47,29 +48,30 @@ def train_policy(num_of_envs, log_relative_path, maximum_episode_length,
     return
 
 
-total_time_steps_per_update = 1000000
-num_of_envs = 20
-log_relative_path = 'baseline_picking_ppo'
-maximum_episode_length = 600
-skip_frame = 3
-seed_num = 0
-task_name = 'picking'
-ppo_config = {"gamma": 0.99,
-              "n_steps": 600,
-              "ent_coef": 0.01,
-              "learning_rate": 0.00025,
-              "vf_coef": 0.5,
-              "max_grad_norm": 0.5,
-              "nminibatches": 4,
-              "noptepochs": 4,
-              "tensorboard_log": log_relative_path}
-train_policy(num_of_envs=num_of_envs,
-             log_relative_path=log_relative_path,
-             maximum_episode_length=maximum_episode_length,
-             skip_frame=skip_frame,
-             seed_num=seed_num,
-             ppo_config=ppo_config,
-             total_time_steps=60000000,
-             validate_every_timesteps=1000000,
-             task_name=task_name)
+if __name__ == '__main__':
+    total_time_steps_per_update = 1000000
+    num_of_envs = 20
+    log_relative_path = 'baseline_picking_ppo'
+    maximum_episode_length = 600
+    skip_frame = 3
+    seed_num = 0
+    task_name = 'picking'
+    ppo_config = {"gamma": 0.99,
+                  "n_steps": 600,
+                  "ent_coef": 0.01,
+                  "learning_rate": 0.00025,
+                  "vf_coef": 0.5,
+                  "max_grad_norm": 0.5,
+                  "nminibatches": 4,
+                  "noptepochs": 4,
+                  "tensorboard_log": log_relative_path}
+    train_policy(num_of_envs=num_of_envs,
+                 log_relative_path=log_relative_path,
+                 maximum_episode_length=maximum_episode_length,
+                 skip_frame=skip_frame,
+                 seed_num=seed_num,
+                 ppo_config=ppo_config,
+                 total_time_steps=60000000,
+                 validate_every_timesteps=1000000,
+                 task_name=task_name)
 
