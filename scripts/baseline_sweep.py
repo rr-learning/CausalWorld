54d5f2c33234d23dbd9a2db8716ba32355f8ea70
ftraeuble <ftraeuble@outlook.com>
2020-06-28 13:40:31 +0200

    Initiate baselines tuning

--- scripts/baseline_sweep.py
+++ scripts/baseline_sweep.py
@@ -28,7 +28,7 @@ from stable_baselines.ddpg.noise import NormalActionNoise
 world_seed = 0
 num_of_envs = 20
 
-NUM_RANDOM_SEEDS = 5
+NUM_RANDOM_SEEDS = 2
 NET_LAYERS = [256, 256]
 
 
@@ -205,8 +205,8 @@ def train_model_num(model_settings, output_path):
     if model_settings['algorithm'] == 'PPO':
         model, env = get_PPO_model(model_settings, model_path)
         num_of_active_envs = num_of_envs
-        total_time_steps = 40000000
-        validate_every_timesteps = 2000000
+        total_time_steps = int(40000000)
+        validate_every_timesteps = int(2000000)
     elif model_settings['algorithm'] == 'SAC':
         model, env = get_SAC_model(model_settings, model_path)
         num_of_active_envs = 1
