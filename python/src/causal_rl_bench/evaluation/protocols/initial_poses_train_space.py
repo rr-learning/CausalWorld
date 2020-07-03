e03ca5c77ddb30ceca25bcd1739734c5ab00c068
ftraeuble <ftraeuble@outlook.com>
2020-07-03 15:00:01 +0200

    Add protocols

---
+++ python/src/causal_rl_bench/evaluation/protocols/initial_poses_train_space.py
@@ -0,0 +1,22 @@
+from causal_rl_bench.evaluation.protocol import Protocol
+import numpy as np
+
+
+class InitialPosesTrainSpace(Protocol):
+    def __init__(self):
+        super().__init__('initial_poses_train_space')
+
+    def get_intervention(self, episode, timestep):
+        if timestep == 0:
+            intervention_dict = dict()
+            intervention_space = self.env._task._training_intervention_spaces
+            for rigid_object in self.env._task._stage._rigid_objects:
+                if rigid_object in intervention_space and \
+                        'cartesian_position' in intervention_space[rigid_object]:
+                    intervention_dict[rigid_object] = dict()
+                    intervention_dict[rigid_object]['cartesian_position'] = \
+                        np.random.uniform(intervention_space[rigid_object]['cartesian_position'][0],
+                                          intervention_space[rigid_object]['cartesian_position'][1])
+            return intervention_dict
+        else:
+            return None
