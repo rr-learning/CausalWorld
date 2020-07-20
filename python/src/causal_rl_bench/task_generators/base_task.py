bb1496d9504a0547703435f5f2267768155189b8
ftraeuble <ftraeuble@outlook.com>
2020-07-20 14:22:21 +0200

    remove task_reset

--- python/src/causal_rl_bench/task_generators/base_task.py
+++ python/src/causal_rl_bench/task_generators/base_task.py
@@ -632,9 +632,9 @@ class BaseTask(object):
         :return:
         """
         self._robot.clear()
-        if interventions_dict is None:
-            reset_observation_space_signal = \
-                self.restore_state(self._current_starting_state)
+        reset_observation_space_signal = \
+            self.restore_state(self._current_starting_state)
+
         self._task_solved = False
         success_signal = None
         interventions_info = None
