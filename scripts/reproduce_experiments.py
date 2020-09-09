6a35b7dbb5eb7c8df12aa197356e9a7bdb4f8b9f
ossamaAhmed <ossama.ahmed@mail.mcgill.ca>
2020-09-09 15:59:39 +0200

    updated SAC params

--- scripts/reproduce_experiments.py
+++ scripts/reproduce_experiments.py
@@ -97,11 +97,11 @@ def baseline_model(model_num, task):
            'validate_every_timesteps': int(500000),
            'total_time_steps': int(10000000),
            'train_configs': {
-               "gamma": 0.98,
-               "tau": 0.01,
-               "ent_coef": 'auto',
-               "target_entropy": -9,
-               "learning_rate": 0.00025,
+               "gamma": 0.95,
+               "tau": 1e-3,
+               "ent_coef": 1e-3,
+               "target_entropy": 'auto',
+               "learning_rate":  1e-4,
                "buffer_size": 1000000,
                "learning_starts": 1000,
                "batch_size": 256
